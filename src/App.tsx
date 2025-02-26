b7654ef37edf5278dc1306b6701004a4eade8839
Alex <aclive@live.co.uk>
2025-02-26 21:56:01 +0000

    create generic shape component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import "./App.css";
+import Shape from "./components/Shape";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Shape />
+      <div className="quadrant topLeft" />
+      <div className="quadrant topRight" />
+      <div className="quadrant bottomLeft" />
+      <div className="quadrant bottomRight" />
     </div>
   );
 }
--- src/components/Shape.tsx
+++ src/components/Shape.tsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 import "./Shape.css";
 
-function Shape() {
-  return <div className="App"></div>;
+function Shape({}) {
+  let [coordinates, setCoordinates] = useState([x, y]);
+  function asPercentage(value: Number) {
+    return value.toString() + "%";
+  }
+  return (
+    <div
+      draggable="true"
+      onDragEnd={(e) => {
+        console.log(e);
+        setCoordinates([
+          ((e.pageX - 25) / window.innerWidth) * 100,
+          ((e.pageY - 25) / window.innerHeight) * 100,
+        ]);
+      }}
+      style={{
+        left: asPercentage(coordinates[0]),
+        top: asPercentage(coordinates[1]),
+      }}
+    >
+      <polygon className="triangle" />
+    </div>
+  );
 }
 
 export default Shape;
